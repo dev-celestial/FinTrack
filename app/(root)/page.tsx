aba96aa0f1a57a4c592161eff617d36ffc9ad205
SATYAM KUMAR <workmail.celestial@gmail.com>
2024-12-01 01:20:09 +0530

    Components

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
+
 
 const Home = () => {
+    const loggedIn = { firstName: 'User'}
   return (
-    <div>Home</div>
+    <section className="home">
+        <div className="home-content">
+            <header className="home-header">
+                <HeaderBox
+                type="greeting"
+                title="welcome"
+                user={loggedIn?.firstName || 'Guest'}
+                subtext="Access and manage your account and transactions efficiently."
+                />
+             <TotalBalanceBox 
+            accounts={1}
+            totalBanks={1}
+            totalCurrentBalance={1250}
+          />
+
+            </header>
+        </div>
+    </section>
   )
 }
 
