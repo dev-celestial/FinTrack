5156ed54f12242e17a7c47dee1c1a2b7df7143ce
SATYAM KUMAR <workmail.celestial@gmail.com>
2024-12-02 07:06:30 +0530

    Appwrite

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -20,7 +20,9 @@ import {
 import { Input } from "@/components/ui/input"
 import CustomInput from '@/constants/CustomInput';
 import { authFormSchema } from '@/lib/utils';
-import { Loader2 } from 'lucide-react';
+import { Contact, Loader2 } from 'lucide-react';
+import { useRouter } from 'next/navigation';
+import { signIn, signUp } from '@/lib/actions/user.actions';
 
 
 const formSchema = z.object({
@@ -28,12 +30,16 @@ const formSchema = z.object({
 })
 
 const AuthForm = ({type}: {type: string}) => {
+  const router = useRouter();
     const [user, setUser] = useState(null);
     const [isLoading, setIsLoading] = useState(false);
+
+    const formSchema = authFormSchema(type);
+
     
     // 1. Define your form.
-const form = useForm<z.infer<typeof authFormSchema>>({
-    resolver: zodResolver(authFormSchema),
+const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
     defaultValues: {
       email: "",
       password: '',
@@ -42,10 +48,43 @@ const form = useForm<z.infer<typeof authFormSchema>>({
   })
 
     //2. Submit handler
-    function onSubmit(values: z.infer<typeof formSchema>){
-        setIsLoading(true)
-        console.log(values)
+    const onSubmit = async (data: z.infer<typeof formSchema>) => {
+      setIsLoading(true);
+
+      try {
+        // Sign up with Appwrite & create plaid token
+        
+        if(type === 'sign-up') {
+          const userData = {
+            firstName: data.firstName!,
+            lastName: data.lastName!,
+            address1: data.address1!,
+            city: data.city!,
+            state: data.state!,
+            postalCode: data.postalCode!,
+            dateOfBirth: data.dateOfBirth!,
+            contact: data.contact!,
+            email: data.email,
+            password: data.password
+          }
+          const newUser = await signUp(userData);
+
+          setUser(newUser);
+        }
+
+        if(type === 'sign-in') {
+          const response = await signIn({
+            email: data.email,
+            password: data.password,
+          })
+
+          if(response) router.push('/')
+        }
+      } catch (error) {
+        console.log(error);
+      } finally {
         setIsLoading(false);
+      }
     }
   return (
     <section className="auth-form">
@@ -85,6 +124,25 @@ const form = useForm<z.infer<typeof authFormSchema>>({
             <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
         
+      {type === 'sign-up' && (
+                <>
+                  <div className="flex gap-4">
+                    <CustomInput control={form.control} name='firstName' label="First Name" placeholder='Enter your first name' />
+                    <CustomInput control={form.control} name='lastName' label="Last Name" placeholder='Enter your first name' />
+                  </div>
+                  <CustomInput control={form.control} name='address1' label="Address" placeholder='Enter your specific address' />
+                  <CustomInput control={form.control} name='city' label="City" placeholder='Enter your city' />
+                  <div className="flex gap-4">
+                    <CustomInput control={form.control} name='state' label="State" placeholder='Example: DELHI' />
+                    <CustomInput control={form.control} name='postalCode' label="Postal Code" placeholder='Example: 110011' />
+                  </div>
+                  <div className="flex gap-4">
+                    <CustomInput control={form.control} name='dateOfBirth' label="Date of Birth" placeholder='YYYY-MM-DD' />
+                    <CustomInput control={form.control} name='ContactNo' label="Contact" placeholder='' />
+                  </div>
+                </>
+              )}
+
         <CustomInput
         control={form.control}
         name='email' label='Email'
@@ -96,7 +154,7 @@ const form = useForm<z.infer<typeof authFormSchema>>({
         placeholder='Enter your Password'
         />
 
-
+        <div className='flex flex-col gap-4' >
         <Button type="submit" disabled={isLoading}
         className='form-btn' >
             {isLoading ? (
@@ -107,15 +165,17 @@ const form = useForm<z.infer<typeof authFormSchema>>({
                 </>
             ): type === 'sign-in' ? 'Sign In' : 'Sign Up'}
         </Button>
+        </div>
+        
       </form>
     </Form>
 
     <footer className= "flex justify-center gap-1" >
-            <p>
+            <p className='text-14 font-normal text-gray-600' >
                {type === 'sign-in'? "Don't have an account?" : "Already have an account?"} 
             </p>
             <Link href={type === 'sign-in' ? '/sign-up' : 'sign-in'} className="form-link">
-              {type === 'sign-in' ? 'sign up' : 'Sign in'}
+              {type === 'sign-in' ? 'Sign up' : 'Sign in'}
             </Link>
     </footer>
             </>
